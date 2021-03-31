a8436f809e0aad14a45db46d4ec9fd30066caf40
eshemtov <78756754+eshemtov@users.noreply.github.com>
2021-03-30 22:19:32 -0400

    seventh commit

---
+++ exercises/jquery/assets/main.js
@@ -0,0 +1,62 @@
+$(function() {
+
+	$(".cls-1").click(function() {
+		let colors = ["lightsteelblue", "thistle", "darkturquoise", "gold", "lightcoral", "#7bb29a"]
+		let i = Math.floor(Math.random()* colors.length);
+		$(".cls-1").css("fill", colors[i]);
+	});
+
+	$(".cls-2").click(function() {
+		let colors = ["lightsteelblue", "thistle", "darkturquoise", "gold", "lightcoral", "#7bb29a"]
+		let i = Math.floor(Math.random()* colors.length);
+		$(".cls-2").css("fill", colors[i]);
+	});
+
+	$(".cls-3").click(function() {
+		let colors = ["lightsteelblue","thistle","darkturquoise","gold","lightcoral","#7bb29a"]
+		let i = Math.floor(Math.random()* colors.length);
+		$(".cls-3").css("fill", colors[i]);
+	});
+
+	$(".cls-4").click(function() {
+		let colors = ["lightsteelblue","thistle","darkturquoise","gold","lightcoral","#7bb29a"]
+		let i = Math.floor(Math.random()* colors.length);
+		$(".cls-4").css("fill", colors[i]);
+	});
+
+	$(".cls-5").click(function() {
+		let colors = ["lightsteelblue","thistle","darkturquoise","gold","lightcoral","#7bb29a"]
+		let i = Math.floor(Math.random()* colors.length);
+		$(".cls-5").css("fill", colors[i]);
+	});
+
+	$(".cls-6").click(function() {
+		let colors = ["lightsteelblue","thistle","darkturquoise","gold","lightcoral","#7bb29a"]
+		let i = Math.floor(Math.random()* colors.length);
+		$(".cls-6").css("fill", colors[i]);
+	});
+
+	$(".cls-7").click(function() {
+		let colors = ["lightsteelblue","thistle","darkturquoise","gold","lightcoral","#7bb29a"]
+		let i = Math.floor(Math.random()* colors.length);
+		$(".cls-7").css("fill", colors[i]);
+	});
+
+	$(".cls-8").click(function() {
+		let colors = ["lightsteelblue","thistle","darkturquoise","gold","lightcoral","#7bb29a"]
+		let i = Math.floor(Math.random()* colors.length);
+		$(".cls-8").css("fill", colors[i]);
+	});
+
+	$(".cls-9").click(function() {
+		let colors = ["lightsteelblue","thistle","darkturquoise","gold","lightcoral","#7bb29a"]
+		let i = Math.floor(Math.random()* colors.length);
+		$(".cls-9").css("fill", colors[i]);
+	});
+
+	$(".cls-10").click(function() {
+		let colors = ["lightsteelblue","thistle","darkturquoise","gold","lightcoral","#7bb29a"]
+		let i = Math.floor(Math.random()* colors.length);
+		$(".cls-10").css("fill", colors[i]);
+	});
+});
